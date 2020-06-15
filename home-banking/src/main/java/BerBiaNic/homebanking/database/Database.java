0e7bb89f5b066ff3d86f42139f7bacfba583abd9
ottimax <pepsy__97@hotmail.it>
2020-06-15 11:56:21 +0200

    Creazione classe Database

--- home-banking/src/main/java/BerBiaNic/homebanking/database/Database.java
+++ home-banking/src/main/java/BerBiaNic/homebanking/database/Database.java
@@ -1,5 +1,44 @@
 package BerBiaNic.homebanking.database;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
 public class Database {
 
+
+	private static Connection conn = null;
+	private final String DELIMITATOR = System.getProperty("file.separator");
+	private final String PATH_PROPERTIES = "src" + DELIMITATOR + "main" + DELIMITATOR + "resources" + DELIMITATOR + "config.properties";
+	private static String DB_HOST;
+	private static String DB_USER;
+	private static String DB_PASS;
+	
+	
+	public Database() throws IOException {
+		Properties p;
+		FileReader in = new FileReader(PATH_PROPERTIES);
+		p = new Properties();
+		p.load(in);
+		
+		DB_HOST = p.getProperty("db.host");
+		DB_USER = p.getProperty("db.user");
+		DB_PASS = p.getProperty("db.password");
+	}
+	
+	public static Connection getConnection() {
+		try {
+			conn = DriverManager.getConnection("jdbc:mysql://" +Database.DB_HOST + ":3306/sql7347775", Database.DB_USER, Database.DB_PASS);
+			return conn;
+		}catch (SQLException ex) {
+			System.out.println("SQLException: " + ex.getMessage());
+			System.out.println("SQLState: " + ex.getSQLState());
+			System.out.println("VendorError: " + ex.getErrorCode());
+			return null;
+		}
+	}
 }
--- home-banking/src/main/java/BerBiaNic/homebanking/main/Main.java
+++ home-banking/src/main/java/BerBiaNic/homebanking/main/Main.java
@@ -1,9 +1,26 @@
 package BerBiaNic.homebanking.main;
 
-public class Main {
 
-	public static void main(String[] args) {
+import java.io.IOException;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import BerBiaNic.homebanking.database.Database;
+
+public class Main {
+	public static void main(String[] args) throws SQLException, IOException {
 		
+		
+		
+		Database db = new Database();
+		
+		Statement stmt = (Statement) db.getConnection().createStatement();
+		ResultSet rs = stmt.executeQuery("SELECT * FROM cliente");
+		
+		while(rs.next())
+			System.out.println("nome: " + rs.getString("nome"));
+	
 
 	}
 
