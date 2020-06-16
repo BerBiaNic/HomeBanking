ecd73a026933affd00592b5f9d957ff3eeb04ce1
anduril92 <giovanni.92.n@hotmail.it>
2020-06-16 18:38:36 +0200

    Prove Dao fallite

--- home-banking/src/main/java/BerBiaNic/homebanking/dao/DaoCliente.java
+++ home-banking/src/main/java/BerBiaNic/homebanking/dao/DaoCliente.java
@@ -1,37 +1,72 @@
 package BerBiaNic.homebanking.dao;
 
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.Future;
 
-public class DaoCliente implements Dao {
+import BerBiaNic.homebanking.db.Database;
+import BerBiaNic.homebanking.entity.Cliente;
+
+public class DaoCliente implements Dao <Cliente,String> {
 
 	@Override
-	public Future getOne(Object primaryKey) {
+	public Future<Cliente> getOne(String primaryKey) {
+		String query = "SELECT * FROM cliente WHERE codice_fiscale = ?";
+		CompletableFuture<Cliente> cliente = CompletableFuture.supplyAsync(() -> {
+			
+			try {
+				PreparedStatement ps = Database.getConnection().prepareStatement(query);
+				ps.setString(1, primaryKey);
+				ResultSet rs = ps.executeQuery();
+				String cf = rs.getString(1); 
+				String cm =  rs.getString("cognome");
+				String nm = rs.getString("nome"); 
+				String ctt = rs.getString("citta_di_nascita");
+				Date dn = rs.getDate("data_di_nascita"); 
+				int nt = rs.getInt("numero_di_telefono");
+				String ind =  rs.getString("indirizzo_di_residenza");
+				String cr = rs.getString("citta_di_residenza");
+				Cliente c = new Cliente(cf, cm, nm, ctt, dn, nt, ind, cr);
+				System.out.println(cf +", "+ cm +", "+ nm+", "+ ctt+", "+ dn+", "+ nt+", "+ ind+", "+ cr);
+				
+				System.out.println(c);
+				return c;
+			} catch (SQLException e) {
+				e.printStackTrace();
+				return null;
+			}
+		});
+		return cliente;
+	}
+
+	@Override
+	public Future<List<Cliente>> getAll() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
-	public Future getAll() {
+	public Future<Cliente> insert(Cliente element) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
-	public Future insert(Object element) {
+	public Future<Integer> delete(String primaryKey) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
-	public Future delete(Object primaryKey) {
+	public Future<Cliente> update(Cliente element) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	@Override
-	public Future update(Object element) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+
 
 }
--- home-banking/src/main/java/BerBiaNic/homebanking/main/Main.java
+++ home-banking/src/main/java/BerBiaNic/homebanking/main/Main.java
@@ -5,18 +5,31 @@ import java.io.IOException;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
 
+import BerBiaNic.homebanking.dao.*;
 import BerBiaNic.homebanking.db.Database;
+import BerBiaNic.homebanking.entity.*;
 
 public class Main {
-	public static void main(String[] args) throws SQLException, IOException {
+	public static void main(String[] args) throws SQLException, IOException, InterruptedException, ExecutionException {
 		
 		Database db = new Database();
 		
 		Statement stmt = (Statement) db.getConnection().createStatement();
 		ResultSet rs = stmt.executeQuery("SELECT * FROM cliente");
 		
-		while(rs.next())
+		while(rs.next()) {
 			System.out.println("nome: " + rs.getString("nome"));
+			System.out.println();
+			System.out.println("numero: " + rs.getInt("numero_di_telefono"));
+		}
+		
+		Dao<Cliente,String> daoAccount = new DaoCliente();
+		Future<Cliente> futureCliente = daoAccount.getOne("");
+		System.out.println(futureCliente.get());
+		
+		
 	}
 }
