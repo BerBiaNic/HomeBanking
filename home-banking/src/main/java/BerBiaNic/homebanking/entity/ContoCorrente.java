f7bfb020a2a764dab7b45e263019d273155810c9
antobert91 <61738546+antobert91@users.noreply.github.com>
2020-06-21 13:42:28 +0200

    Aggiunte validazioni entity

--- home-banking/src/main/java/BerBiaNic/homebanking/entity/ContoCorrente.java
+++ home-banking/src/main/java/BerBiaNic/homebanking/entity/ContoCorrente.java
@@ -1,5 +1,15 @@
 package BerBiaNic.homebanking.entity;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
+import javax.json.bind.JsonbBuilder;
+import javax.json.bind.JsonbConfig;
+import javax.json.bind.annotation.JsonbCreator;
+import javax.json.bind.annotation.JsonbProperty;
+import javax.json.bind.config.PropertyVisibilityStrategy;
+
+import BerBiaNic.homebanking.exceptions.InputValidationException;
 
 public class ContoCorrente{
 
@@ -9,7 +19,29 @@ public class ContoCorrente{
 	private double saldo_contabile;
 	private Account account;
 
-	public ContoCorrente(int numero, String iban, double saldo_disponibile, double saldo_contabile, Account account) {
+	@JsonbCreator
+	public ContoCorrente(@JsonbProperty("numero") int numero, @JsonbProperty("iban") String iban, @JsonbProperty("saldo_disponibile") double saldo_disponibile, 
+			@JsonbProperty("saldo_contabile") double saldo_contabile, @JsonbProperty("account") Account account) throws InputValidationException {
+
+		if(numero < 100000)
+			throw new InputValidationException("Numero conto corrente");
+
+		if (iban == null || iban.isBlank())
+			throw new InputValidationException("IBAN conto corrente");
+		if(iban.length() != 31)
+			throw new InputValidationException("IBAN conto corrente non valido. Caratteri richiesti 27, inseriti: ", iban.length());
+		if(!iban.matches("IT+\\d{2}+[a-zA-Z]+\\d{22}"))
+			throw new InputValidationException("Formato IBAN conto corrente (esempio inserimento  IT28W8000000292100645211151)");
+
+		if(saldo_contabile < 0)
+			throw new InputValidationException("Saldo contabile");
+
+		if(saldo_disponibile < 0)
+			throw new InputValidationException("Saldo disponibile");
+
+		if (account == null)
+			throw new InputValidationException("Account associato al conto corrente");
+
 		this.numero = numero;
 		this.iban = iban;
 		this.saldo_disponibile = saldo_disponibile;
@@ -82,8 +114,18 @@ public class ContoCorrente{
 				+ "numero = " + numero + ", \tiban = " + iban + ", \tsaldo_disponibile = " + saldo_disponibile
 				+ ", \tsaldo_contabile = " + saldo_contabile + "\n";
 	}	
-	
-	
-	
 
+	public String toJson() {
+		JsonbConfig config = new JsonbConfig().withPropertyVisibilityStrategy(new PropertyVisibilityStrategy() {
+			@Override
+			public boolean isVisible(Method arg0) {
+				return false;
+			}
+			@Override
+			public boolean isVisible(Field arg0) {
+				return true;
+			}
+		});
+		return JsonbBuilder.newBuilder().withConfig(config).build().toJson(this);
+	}
 }
